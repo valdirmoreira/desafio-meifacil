84997e2d07058a90616c60c85d8eb0f04ebfc417
Valdir Moreira Silveira Junior <valdir.moreira.junior@gmail.com>
2019-06-25 19:03:24 -0300

    Criação do projeto que será responsavél pela injenção de dependência

---
+++ src/MeiFacil.Payment.Infrastructure.CrossCutting.IoC/NativeInjectorBootStrapper.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MeiFacil.Payment.Infrastructure.CrossCutting.IoC
+{
+    public class NativeInjectorBootStrapper
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+        }
+    }
+}

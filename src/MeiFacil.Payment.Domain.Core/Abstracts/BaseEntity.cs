1f9343bf1f9ec172d9da25577ceea026a03401f0
Valdir Moreira Silveira Junior <valdir.moreira.junior@gmail.com>
2019-06-25 18:25:14 -0300

    Inicia o desenvolvimento da camada de domínio da aplicação

---
+++ src/MeiFacil.Payment.Domain.Core/Abstracts/BaseEntity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MeiFacil.Payment.Domain.Core.Abstracts
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; protected set; }
+        public override bool Equals(object obj)
+        {
+            var compareTo = obj as BaseEntity;
+
+            if (ReferenceEquals(this, compareTo)) return true;
+            if (ReferenceEquals(null, compareTo)) return false;
+
+            return Id.Equals(compareTo.Id);
+        }
+
+        public static bool operator ==(BaseEntity a, BaseEntity b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(BaseEntity a, BaseEntity b)
+        {
+            return !(a == b);
+        }
+
+        public override int GetHashCode()
+        {
+            return (GetType().GetHashCode() * 907) + Id.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return GetType().Name + " [Id=" + Id + "]";
+        }
+    }
+}
---
+++ src/MeiFacil.Payment.Domain/Entities/Payment.cs
@@ -0,0 +1,6 @@
+﻿namespace MeiFacil.Payment.Domain.Entities
+{
+    public class Payment
+    {
+    }
+}
